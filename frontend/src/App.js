b64be5f098ce1ed3806dd5a51d82205edbcdfea3
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2023-03-22 20:08:48 +0800

    tweet card initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,23 @@ import './App.css';
 import { NavigationBar } from './Navbar.js';
 import { Route, Navigate } from 'react-router-dom';
 import { Login } from './Login';
+import { TweetCard } from './TweetCard'
 
 function App() {
 
   return (
     <div>
-    {/* //   <div style={{ textAlign: 'center' }}>
-    //     <NavigationBar page={"login"}/>
-    //     <h1 style={{ textAlign: "center" }}>ChatU is under development. </h1>
-    //     &nbsp;<img src='./logo_colorful.svg' style={{ height: 200, alignItems: "center" }} className='center' alt='logo'></img>&nbsp;
-    //   </div> */}
+      <div style={{ textAlign: 'center' }}>
+        <NavigationBar page={"login"} />
+        <h1 style={{ textAlign: "center" }}>ChatU is under development. </h1>
+        &nbsp;<img src='./logo_colorful.svg' style={{ height: 200, alignItems: "center" }} className='center' alt='logo'></img>&nbsp;
+      </div>
 
       <BrowserRouter>
         {/* <NavigationBar page='login' /> */}
+        <TweetCard />
         <Routes>
-          { <Route path="/Login" element={<Login />} /> }
+          {<Route path="/Login" element={<Login />} />}
           { /*<Route path="/Login" element={<Navigate to="/Login" />} /> */}
         </Routes>
       </BrowserRouter>

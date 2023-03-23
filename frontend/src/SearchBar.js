95deb922fc332f6546e5efd36003ad6232a34f1a
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2023-03-23 15:38:08 +0800

    SearchBar Bug Report
    icon is not displayed in user and comment search bar.

--- frontend/src/SearchBar.js
+++ frontend/src/SearchBar.js
@@ -1,43 +1,49 @@
 import './SearchBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-export function SearchBar({page}){
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+export function SearchBar({ page }) {
     let Bar;
-    if(page === 'tweet'){
-        Bar = (
-            <div class="container">
-                <div class="box">
-                <i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
-                <form name="search">
-                    <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();" placeholder='#ID/content'/>
-                </form>
-                </div>
-                
-            </div>
-        )
-    }
-    if (page === 'user'){
-        Bar = (
-            <div class="container">
-                <div class="box">
-                <form name="search">
-                    <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();"placeholder='#'/>
-                </form>
-                <i class="faMagnifyingGlass" ></i>
 
+    // Search bar for tweet
+    if (page === 'tweet') {
+        Bar = (
+            <div class="container">
+                <div class="box">
+                    <i><FontAwesomeIcon icon={faMagnifyingGlass} /></i>
+                    <form name="search">
+                        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();" placeholder='#ID/content' />
+                    </form>
                 </div>
             </div>
         )
     }
-    if(page === 'comment'){
+
+    // Search bar for user
+    // !!! Search Icon is not shown
+    else if (page === 'user') {
         Bar = (
             <div class="container">
                 <div class="box">
-                <form name="search">
-                    <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();"placeholder='#'/>
-                </form>
-                <i class="fa-solid fa-magnifying-glass"></i>
+                    <form name="search">
+                        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();" placeholder='#' />
+                    </form>
+                    <i class="faMagnifyingGlass" ></i>
+                </div>
+            </div>
+        )
+    }
 
+    // Search bar for comment
+    // !!! Search Icon is not shown
+    else if (page === 'comment') {
+        Bar = (
+            <div class="container">
+                <div class="box">
+                    <form name="search">
+                        <input type="text" class="input" name="txt" onmouseout="this.value = ''; this.blur();" placeholder='#' />
+                    </form>
+                    <i class="fa-solid fa-magnifying-glass"></i>
                 </div>
             </div>
         )

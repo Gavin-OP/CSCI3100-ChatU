6dc9cf1862edf91584d8a61ba19a885f8b105e98
HaopengDENG <80114677+HaopengDENG@users.noreply.github.com>
2023-03-24 21:15:02 +0800

    Update Homepage.js
    Cleaned out some test script

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -36,8 +36,8 @@ export function Homepage() {
 
 <div class="wrapper">
   <div class="col col1">
-     <div class="col-content">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     <div class="col-content"> 
+       {/*Empty space in which additional content can be put in the future */}
      </div>
   </div>
   <div class="col col2">

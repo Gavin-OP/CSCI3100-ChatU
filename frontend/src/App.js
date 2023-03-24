2faf1707c96404ec48b4fa24fc2100afd99bf2db
zhgu142 <1155141578@link.cuhk.edu.hk>
2023-03-24 22:23:57 +0800

    Add post and tweet/retweet page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,10 @@ import { Login } from './Login';
 import { Test } from './Test'
 import { Signup } from './Signup';
 import { Adminpage } from './Adminpage';
-import {Homepage} from './Homepage';
+import { Homepage } from './Homepage';
 import { UnderDevelopment } from './UnderDevelopment';
-
+import { Post } from './Post'
+import { Tweetpage } from './Tweetpage'
 function App() {
   return (
     <div>
@@ -25,6 +26,8 @@ function App() {
           <Route path="/adminpage/comment" element={<Adminpage page="comment" />} />
           <Route path="/homepage" element={<Homepage page="homepage"/>} />
           <Route path='/test' element={<Test />} />
+          <Route path='/post' element={<Post />} />
+          <Route path='/tweetpage' element={<Tweetpage />} />
           <Route path='/*' element={<UnderDevelopment />} />
         </Routes>
       </BrowserRouter>

93167c14f1864d224ac5b25dec01b3d28016a411
LIUUUUUZ <93988538+LIUUUUUZ@users.noreply.github.com>
2023-03-25 15:16:32 +0800

    Change some file name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {
   Routes,
   Route,
 } from 'react-router-dom';
-
+// Remember to name the file correctly
 import './App.css';
 import { Login } from './Login';
 import { Test } from './Test'

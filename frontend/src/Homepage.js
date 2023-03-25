0abe7a4b8e20291ad9ffcc98d10bb9615916e5fe
HaopengDENG <80114677+HaopengDENG@users.noreply.github.com>
2023-03-25 15:45:40 +0800

    Deployed UserRecomBox
    Deployed UserRecomBox

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -7,7 +7,7 @@ import { faPlus, faRotateRight } from '@fortawesome/free-solid-svg-icons'
 import React from "react";
 import ScrollToTop from 'react-scroll-to-top';
 import ReactDOM from 'react-dom';
-
+import { UserRecomBox } from './UserRecommendBox';
 
 
 const tweet_data = {
@@ -60,69 +60,11 @@ export function Homepage() {
                <div class="col col3">
                     <div class="col-content">
                          <br/><br/> &nbsp; &nbsp; <button class="write-post-button"><i class="fa fa-plus"></i></button>
-                              <div class="user-rec-font">  <br/> <br/> &nbsp; &nbsp; <button class="reload-button"><i class="fa fa-rotate-right"></i> </button>
-                              &nbsp; User Recommendation 
+                              <div class="user-rec-font">  <br/> <br/> &nbsp; &nbsp; 
+                              User Recommendation 
                               </div>
                               <br/> <br/> <br/> <br/> 
-                     <div class="user-rec-box"> 
-                            <div className="user-rec-avatar">
-                                <img src='./avatar2.png' alt="Avatar2" class="center" />
-                            </div>
-                            <div className="user-rec-text"> User2  </div>
-                            <div className='user-rec-follow'>
-                                <button className="user-rec-follow-button">follow</button>
-                            </div>
-                        </div>
-
-                        <br /> <br />
-
-                        <div class="user-rec-box">
-                            <div className="user-rec-avatar">
-                                <img src='./avatar2.png' alt="Avatar2" class="center" />
-                            </div>
-                            <div className="user-rec-text"> User3  </div>
-                            <div className='user-rec-follow'>
-                                <button className="user-rec-follow-button">follow</button>
-                            </div>
-                        </div>
-
-                        <br /> <br />
-
-                        <div class="user-rec-box">
-                            <div className="user-rec-avatar">
-                                <img src='./avatar2.png' alt="Avatar2" class="center" />
-                            </div>
-                            <div className="user-rec-text"> User4  </div>
-                            <div className='user-rec-follow'>
-                                <button className="user-rec-follow-button">follow</button>
-                            </div>
-                        </div>
-
-                        <br /> <br />
-
-                        <div class="user-rec-box">
-                            <div className="user-rec-avatar">
-                                <img src='./avatar2.png' alt="Avatar2" class="center" />
-                            </div>
-                            <div className="user-rec-text"> User5  </div>
-                            <div className='user-rec-follow'>
-                                <button className="user-rec-follow-button">follow</button>
-                            </div>
-                        </div>
-
-                        <br /> <br />
-
-                        <div class="user-rec-box">
-                            <div className="user-rec-avatar">
-                                <img src='./avatar2.png' alt="Avatar2" class="center" />
-                            </div>
-                            <div className="user-rec-text"> User6  </div>
-                            <div className='user-rec-follow'>
-                                <button className="user-rec-follow-button">follow</button>
-                            </div>
-                        </div>
-
-                        <br /> <br />
+                              <UserRecomBox />
 
                     </div>
                 </div>

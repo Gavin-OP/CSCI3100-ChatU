7d699adfd8efb5aa1484d062fd621c029464cf84
HaopengDENG <80114677+HaopengDENG@users.noreply.github.com>
2023-03-25 13:25:26 +0800

    Added route path for all subpages of personal page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,11 @@ function App() {
           <Route path="/adminpage/user" element={<Adminpage page="user" />} />
           <Route path="/adminpage/comment" element={<Adminpage page="comment" />} />
           <Route path="/homepage" element={<Homepage />} />
-          <Route path="/personalpage" element={<Personalpage />} />
+          <Route path="/personalpage/tweet" element={<Personalpage page="tweet" />} />
+          <Route path="/personalpage/fav" element={<Personalpage page="favourite" />} />
+          <Route path="/personalpage/fowllowing" element={<Personalpage page="following" />} />
+          <Route path="/personalpage/fans" element={<Personalpage page="fans" />} />
+          <Route path="/personalpage/blacklist" element={<Personalpage page="blacklist" />} />
           <Route path='/test' element={<Test />} />
           <Route path='/post' element={<Post />} />
           <Route path='/retweet' element={<Retweet />} />

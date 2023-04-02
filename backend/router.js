b59415db608932dc2c53538c4e62abc4e9da99fa
Gavin-OP <116365822+Gavin-OP@users.noreply.github.com>
2023-04-02 18:35:50 +0800

    create schema files

--- backend/router.js
+++ backend/router.js
@@ -6,9 +6,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 const feedbackRoutes = require('./feedback')
 const userRoutes = require('./user')

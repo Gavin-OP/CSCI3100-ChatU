a1c7bee382b4aab5c4c0fe70b4245bdaa61b4d86
zhgu142 <1155141578@link.cuhk.edu.hk>
2023-04-05 23:34:00 +0800

    Update Signup and Login
    Now it can work with backend

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -2,42 +2,32 @@ import './Login.css';
 import React from "react";
 import { NavigationBar } from './NavBar';
 export class SignUp extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props);
+        this.state={failure:0};
     }
-    handleSignup = () => {
-        let div = document.getElementById('warning');
-        if (div.classList.contains('d-none')) {
-        }
-        else {
-            div.classList.add('d-none');
-        }
+    handleSignup=(e)=>{
+        e.preventDefault();
         let em = document.getElementById('email').value;
         let password = document.getElementById('pwd').value;
         let user_name = document.getElementById('user-name').value;
-
-        fetch('/user/signUp',
-            {
-                method: 'POST',
-                mode: 'cors',
-                body: JSON.stringify({
-                    email: em,
-                    pwd: password,
-                    username: user_name
-                }),
-                headers: { 'Content-Type': 'application/json' }
-            })
-            .then(res => res.json())
-            .then(res => {
-                console.log(res.message);
-                window.history.pushState('', '', '/home');
-            })
-            .catch(error => {
-                console.log(error);
-                div.classList.remove('d-none');
-            })
+        
+        fetch('/user/signUp', 
+        {method:'POST', 
+        body: JSON.stringify({email: em, pwd: password, username: user_name}),
+        headers: { 'Content-Type': 'application/json' }})
+        .then(res=>res.json())
+        .then(res=>{
+            console.log(res.message);
+            window.location.href="/home";
+            this.setState({failure: 0});
+        })
+        .catch(error=>{
+            console.log(error);
+            document.getElementById('warning').style.display='block';
+        })
     }
-    render() {
+    render(){
         return (
             <div>
                 {/* Navigationbar */}
@@ -51,10 +41,10 @@ export class SignUp extends React.Component {
                     <br />
                     {/* sign-up */}
                     <div className="sign-up">
-                        <form method='post'>
+                        <form>
                             <div className="form-field">
                                 <span className="User-name"></span>
-                                <input id="user-name" type="string" name="user-name" placeholder="User-name" maxLength={20} required />
+                                <input id="user-name" type="string" name="user-name" placeholder="User-name" maxLength={20} required/>
                             </div>
                             <div className="form-field">
                                 <span className="Email"></span>
@@ -62,15 +52,13 @@ export class SignUp extends React.Component {
                             </div>
                             <div className="form-field">
                                 <span className="pw"></span>
-                                <input type="password" name="password" id="pwd" placeholder="Password" minLength={6} maxLength={20} required />
+                                <input type="password" name="password" id="pwd" placeholder="Password" minLength={6} maxLength={20} required/>
                             </div>
-                            <button type="submit" className="btn sign-up-submit">Sign-up</button>
+                            <button type="button" className="btn sign-up-submit" onClick={this.handleSignup}>Sign-up</button>
                             <div className="text-center">
                                 <a href='./Login'>Existed an account?</a>
                             </div>
-                            <div className="text-center d-none alert alert-danger" id='warning'>
-                                Fail to Sign up!
-                            </div>
+                           <div className="text-center alert alert-danger" id='warning' style={{display:'none'}}>Fail to Sign up!</div>
                         </form>
                     </div>
                 </div>

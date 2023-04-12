ae52e5bdea9b22ef07be943b92856fd1815e61e7
HaopengDENG <80114677+HaopengDENG@users.noreply.github.com>
2023-04-12 21:52:39 +0800

    Update search.js

--- backend/search.js
+++ backend/search.js
@@ -6,21 +6,21 @@ const User = require('./userSchema');
 const { json } = require('body-parser');
 
 //Search for tweets by keyword(s)
-// Input: a string, called 'search', that contains the search string inputted by the user
+// Input: a string, called "search", that contains the search string inputted by the user
 router.post('/searchTweet', (req, res) =>{
        const keyword = req.body['search']
 
-       Tweet.find( { "content": { $regex: /.*keyword.*/, $options: 'i' } } )
+       Tweet.find( { content: { $regex: /.*keyword.*/, $options: 'i' } } )
             .then((matchedTweets) => {
-                if(!matchedTweets) {
-                    return res.status(404).json({
-                        message: 'No tweet matches the keyword(s) you gave'
-                    })
-                }
+                    if(matchedTweets === []) {
+                          return res.status(404).json({
+                                message: 'No tweet matches the keyword(s) you gave'
+                          })
+                       }
 
-                    const promises = matchedTweets.map((tweet) => {
-
-                        return {
+                    res.set('Content-Type', 'application/json');
+                    res.json(matchedTweets.map((tweet) => {
+                          return {
                             tweet_id: tweet.tweet_id,
                             content: tweet.content,
                             user: tweet.user,
@@ -31,20 +31,21 @@ router.post('/searchTweet', (req, res) =>{
                             like: tweet.like,
                             dislike: tweet.dislike,
                             tag: tweet.tag
-                        };
-                    });
-
+                          };
+                    }));
+/*
                     Promise.all(promises)
                     .then(tweets => {
                         res.status(200).json(tweets);
                     })
+  */          
             })
             .catch(error => {
                 console.error(`Error retrieving details of tweets that match the keyword ${keyword}:`, error);
                 res.status(500).json({
                     message: 'Failed to retrieve matched tweet(s) from the db.'
-                });
-            });  
+                }); 
+        }); 
 });
 
 module.exports = router;
